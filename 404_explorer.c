c8d7245bbb00a5eeebaf7deeb802917cccded1f2
Young-Owl <goncalorodrigues01@gmail.com>
2022-05-27 01:31:43 +0100

    + Implemented a new reading technique yet to test.

--- 404_explorer.c
+++ 404_explorer.c
@@ -3,32 +3,144 @@
 // Compile and Run
 // pcompile 404_explorer.c mr32.c && ldpic32 -w 404_explorer.hex -p /dev/ttyS3
 
+/**
+ * The function takes in a direction and a velocity and turns the robot in the direction specified by
+ * the direction parameter.
+ * 
+ * @param direction The direction the robot is turning.
+ * @param rotateVel The speed at which the robot will rotate.
+ */
+void turnDir(char direction, int rotateVel)
+{
+    
+    switch(direction)
+    {
+        case 'S':
+        /* The robot is going straight. */
+            break;
+        case 'L':
+        /* Turning the robot to the left. */
+            setVel2(-rotateVel, rotateVel);
+            delay(200);
+            break;
+        case 'R':
+        /* Turning the robot to the right. */
+            setVel2(rotateVel, -rotateVel);
+            delay(200);
+            break;
+        case 'O':
+        /* Turning the robot around. */
+            setVel2(-rotateVel, rotateVel);
+            delay(200);
+            break;     
+    }  
+}
+
+/**
+ * It takes an integer, converts it to binary, and returns an array of the binary digits
+ * 
+ * @param sensorVal the value of the sensor
+ * 
+ * @return The address of the first element of the array.
+ */
+int *sensorBinary(int sensorVal)
+{
+    int i, j, bits_all;
+    int bit[5];
+
+    for(i = 0; i < 5; i++){
+      bit[i] = (sensorVal & 0x01) >> i;
+    }
+    return bit;
+}
+
 int main(void){
     
+    /* Initializing the robot. */
     initPIC32();
     closedLoopControl( true );
     setVel2(0, 0);
     printf("404: Failure Not Found\n\tReady!\n\n");
 
+    /* Declaring the variables that will be used in the program. */
     int sensorRead;
-    int maxVel = 100, normalVel = 50, rotateVel = 40, rotateMinVel = 20;
-
+    int maxVel = 100, normalVel = 50, normalMinVel = 20, rotateVel = 40, rotateMinVel = 30;
+    int *sensorBin;
+    int straight = 0, right = 0, left = 0, deadEnd = 0;
+    char direction;
+    //unsigned int sensorsVal[10];
+        
     while(1)
     {
         printf("Press start to continue\n");
         while(!startButton())
         {
+            /* Reading the line sensors and printing the value. */
             waitTick80ms();
             sensorRead = readLineSensors(0);
-            //printInt(sensorRead, 2 | 5 << 16);
-            //printf("\n");
+            printInt(sensorRead, 2 | 5 << 16);
+            printf("\n");
         }
         do
         {   
+            setVel2(normalVel,normalVel);
             waitTick80ms();
-            sensorRead = readLineSensors(10);
+            sensorRead = readLineSensors(0);
+            /* The 'waitTick80ms()' function waits for 80ms and the 'readLineSensors(0)' function
+            takes 50ms to read the sensors. So, the total time is 130ms. */
 
-            switch(sensorRead){
+            sensorBin = sensorBinary(sensorRead);
+            
+            /* Checking if the robot is at a dead end, a right turn, or a left turn. */
+            if((sensorBin[0] && sensorBin[4]) == 1){
+                /* Dead end reached 
+                        X000X
+                */
+                deadEnd = 1;
+            }
+            else if(sensorBin[0] == 1){
+                /* Right Turn reached 
+                        XXXX1          
+                */
+                right = 1;
+            }
+            else if(sensorBin[4] == 1){
+                /* Left Turn reached 
+                        1XXXX          
+                */
+                left = 1;
+            }
+            
+            setVel2(normalMinVel, normalMinVel);
+            delay(300);
+
+            if(sensorRead == 0x11){
+                /* Intersection reached 
+                        1XXX1          
+                */
+                // TODO
+            }
+            else if(sensorBin[3] == 1 || sensorBin[2] == 1 || sensorBin[1] == 1){
+                /* Straight Line Ahead 
+                        X111X          
+                */
+                straight = 1;
+            }
+
+           
+            /* Checking if the robot is at a dead end, a right turn, or a left turn. */
+            if(right)
+                direction = 'R';
+            else if(straight)
+                direction = 'S';
+            else if(left)
+                direction = 'L';
+            else if(deadEnd)
+                direction = 'O';
+            
+            turnDir(direction,rotateVel);
+
+            /* switch(sensorRead){
                 //  ------ Extreme Warns ------ 
                 case 0x00:          
                 // Possible End of the road     00000
@@ -92,8 +204,7 @@ int main(void){
                 // Left Side                    00001 
                     setVel2(normalVel, -rotateMinVel);
                     break;
-            }
-
+            } */
          
         }   while(!stopButton());
         setVel2(0, 0);
